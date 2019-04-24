7da06db47394910771d87c93081e3bdaf28fb987
rubenmiguelborges <rubenmborges@yahoo.com>
2019-04-24 16:46:20 +0100

    Update model.py

--- model.py
+++ model.py
@@ -285,6 +285,7 @@ class DCGAN(object):
         print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
           % (epoch, config.epoch, idx, batch_idxs,
             time.time() - start_time, errD_fake+errD_real, errG))
+        
 
         if np.mod(counter, 100) == 1:
           if config.dataset == 'mnist':
